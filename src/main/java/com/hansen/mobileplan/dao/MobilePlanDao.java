cda2904ae74e1d87e9e02d932d8d3119cbd4a83f
snehalchavan783 <snehalchavan783@gmail.com>
2021-09-08 11:55:03 +0530

    mobileplandavo class

--- src/main/java/com/hansen/mobileplan/dao/MobilePlanDao.java
+++ src/main/java/com/hansen/mobileplan/dao/MobilePlanDao.java
@@ -1,10 +1,6 @@
 package com.hansen.mobileplan.dao;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import com.hansen.mobileplan.model.MobilePlan;
 
 public interface MobilePlanDao extends PagingAndSortingRepository<MobilePlan, Long> 
